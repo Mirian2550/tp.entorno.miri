f113a2664ef608f9d77acb07a6b6a4035eddf657
Mirian2550 <miri2550@gmail.com>
2022-07-18 22:19:40 -0300

    ejercicio 3

--- scripts/findNames.sh
+++ scripts/findNames.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 echo "ingresaste en findNames"
+
+NOMBRE="^[A-Z]+[a-z]+[a-z]*"
+while IFS= read -r line
+do
+    line=$(echo $line | tr "." " ")
+    line=$(echo $line | tr "," " ")
+    line=$(echo $line | tr "" " ")
+    line=$(echo $line | tr "?" " ")
+    PAL=$(echo $line | tr " " "\n")
+    for p in $PAL
+    do
+        if [[ $p =~ $NOMBRE ]]; then
+            echo "Nombre: $p"
+        fi
+    done
+done < demo.txt
